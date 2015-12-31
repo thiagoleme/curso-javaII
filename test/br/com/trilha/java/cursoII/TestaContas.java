252ac024cac1f7e38443e2951dc1848c669436ba
Thiago Henrique Leme <leme.thiagoh@gmail.com>
2015-12-31 00:19:58 -0200

    Exercicio 5

--- test/br/com/trilha/java/cursoII/TestaContas.java
+++ test/br/com/trilha/java/cursoII/TestaContas.java
@@ -3,8 +3,8 @@ package br.com.trilha.java.cursoII;
 public class TestaContas {
 	public static void main(String[] args) {
 		Conta c = new Conta();
-		ContaCorrente cc = new ContaCorrente();
-		ContaPoupanca cp = new ContaPoupanca();
+		Conta cc = new ContaCorrente();
+		Conta cp = new ContaPoupanca();
 		
 		c.deposita(1000);
 		cc.deposita(1000);
